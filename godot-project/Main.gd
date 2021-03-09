5ba27b55da34673768128ad02e251417264f52d2
limelier <tudor.iacobescu.1@gmail.com>
2021-03-09 12:21:17 +0200

    redesign enemy container

--- godot-project/Main.gd
+++ godot-project/Main.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends Node
 
 
 var selected = null
@@ -10,4 +10,4 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if (Input.)
+	pass
