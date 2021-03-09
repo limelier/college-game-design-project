74aa9b93d5e0c2f49e548512d47ed09280435ecc
limelier <tudor.iacobescu.1@gmail.com>
2021-03-09 10:17:09 +0200

    minor additions

---
+++ godot-project/Main.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+
+var selected = null
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if (Input.)
