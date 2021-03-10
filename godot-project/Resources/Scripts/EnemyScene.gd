c006228c2c4535ac7c0880c9a63ebe75a8661aa8
Constantin Suruniuc <costy.suruniuc@gmail.com>
2021-03-10 20:02:11 +0200

    tweaks

--- godot-project/Resources/Scripts/EnemyScene.gd
+++ godot-project/Resources/Scripts/EnemyScene.gd
@@ -1,7 +1,23 @@
 extends Node2D
 
-
+export (PackedScene) var Projectile
 
 func _ready(): 
-	pass
+	get_attacked()
+	
+	
+func get_attacked():
+	$ProjectilePath/ProjectileSpawnLocation.offset = randi()
+	
+	var proj = Projectile.instance()
+	add_child(proj)
+	
+	var direction = $ProjectilePath/ProjectileSpawnLocation.rotation + PI / 2
+	
+	proj.start($ProjectilePath/ProjectileSpawnLocation.position, direction)
+	direction += rand_range(-PI / 4, PI / 4)
+	proj.rotation = direction
+	
+	# proj.linear_velocity = Vector2(proj.speed, 0)
+	# proj.linear_velocity = proj.linear_velocity.rotated(direction)
 
