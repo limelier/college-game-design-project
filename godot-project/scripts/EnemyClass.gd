3a282e68435a1910f977209dd34bb8f343ac25a6
amaterasuonpc <43759618+amaterasuonpc@users.noreply.github.com>
2021-03-29 23:04:09 +0300

    Modified enemy code

--- godot-project/scripts/EnemyClass.gd
+++ godot-project/scripts/EnemyClass.gd
@@ -1,41 +1,37 @@
 extends RigidBody2D
 
-class_name Enemy
-
-export var min_speed = 100
-export var max_speed = 300
+export var min_speed = 20
+export var max_speed = 100
 
 export (PackedScene) var Bullet
 
 var health
-var projectile_damage 
 var screen_size
-var down_accel 
-var down_min_speed 
-
+var down_min_speed
+var down_accel
 var score_value = 10
+var damage
 signal death
-
-
-func _init(Health,Damage,Accel,MinSpeed):
-	health=Health
-	projectile_damage=Damage
-	down_accel=Accel
-	down_min_speed=MinSpeed
 
 func _ready():
 	$BulletTimer.wait_time = rand_range(1.0, 3.0)
 	screen_size = get_viewport_rect().size
 
 func _physics_process(delta):
+	
 	# bounce enemy at edge of screen
-	if position.x < 0 or position.x >= screen_size.x:
-		linear_velocity.x = -linear_velocity.x
+	#if position.x < 20 or position.x >= screen_size.x-20:
+	#	linear_velocity.x = -linear_velocity.x 
+	#	position += linear_velocity * delta
+	if position.x<20:
+		linear_velocity.x = abs(linear_velocity.x) 
 		position += linear_velocity * delta
-		
+	if position.x >=screen_size.x-20:
+		linear_velocity.x = abs(linear_velocity.x)*(-1) 
+		position += linear_velocity * delta
 	# speed up enemy laterally if not moving
-	if abs(linear_velocity.x) < 5:
-		linear_velocity.x = sign(linear_velocity.x) * 5
+	if abs(linear_velocity.x) < 80:
+		linear_velocity.x += sign(linear_velocity.x)*20
 	
 	# accelerate enemy if not going down
 	if linear_velocity.y < down_min_speed:
---
+++ godot-project/scripts/EnemyTypeBIG.gd
@@ -0,0 +1,51 @@
+extends "res://scripts/EnemyClass.gd"
+
+var sgn=1
+func _init():
+	 health = 70
+	 damage = 20
+	 screen_size
+	 down_accel = 20
+	 down_min_speed = 60
+	 score_value = 1000
+	
+
+func _ready():
+	$BulletTimer.wait_time = rand_range(1.0, 3.0)
+	screen_size = get_viewport_rect().size
+
+func _physics_process(delta):
+	if position.y<screen_size.y/4 and sgn==-1:
+		sgn=1
+		linear_velocity.y += down_accel*sgn
+	if position.y>screen_size.y/2 and sgn==1 :
+		sgn=-1
+		linear_velocity.y += down_accel*sgn
+
+		print("Big enemy close to player")
+	if position.x < 0 or position.x >= screen_size.x:
+		linear_velocity.x = -linear_velocity.x
+		position += linear_velocity * delta
+		
+	# speed up enemy laterally if not moving
+	if abs(linear_velocity.x) < 5:
+		linear_velocity.x = sign(linear_velocity.x) * 5
+	
+	# accelerate enemy if not going down
+	if abs(linear_velocity.y) < down_min_speed:
+		linear_velocity.y += down_accel*sgn*2
+		
+		
+func _on_BulletTimer_timeout():
+	$BulletTimer.wait_time = rand_range(0.7, 2.0)
+	
+	var bullet1 = Bullet.instance()
+	get_parent().add_child(bullet1)
+	bullet1.position = $BulletSpawn.global_position
+
+func _on_BulletTimer2_timeout():
+	$BulletTimer2.wait_time = rand_range(0.7, 2.0)
+	
+	var bullet2 = Bullet.instance()
+	get_parent().add_child(bullet2)
+	bullet2.position = $BulletSpawn2.global_position
